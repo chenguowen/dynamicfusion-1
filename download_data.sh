95f9cad62152e67c71edf52935473cd5ea4edc3d
chenguowen <1240803839@qq.com>
2018-08-14 10:27:21 +0800

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-wget http://lgdv.cs.fau.de/uploads/publications/data/innmann2016deform/umbrella_data.zip
-mkdir -p data/umbrella/depth
-mkdir -p data/umbrella/color
-
-mv umbrella_data.zip data/umbrella
-cd data/umbrella
-unzip umbrella_data.zip
-rm *.txt
-mv *color*.png color/
-mv *depth*.png depth/
-rm umbrella_data.zip
+# https://cloud9.cs.fau.de/index.php/s/46qcNZSNePHx08A
 
